d2d26e07ef980d312ab00912d3c22c1ca23da233
Eduardo Silva <p.eduardo.silva@outlook.com>
2018-11-15 11:55:35 -0200

    Bug fix

--- src/HCI.Core.Helper/HCI.Core.Helper/IoC/ServicesRegister.cs
+++ src/HCI.Core.Helper/HCI.Core.Helper/IoC/ServicesRegister.cs
@@ -9,26 +9,16 @@ namespace HCI.Core.Helper.IoC
     /// </summary>
     public static class ServicesRegister
     {
+
         /// <summary>
         /// Add services as scope
         /// </summary>
         /// <param name="container">service container</param>
         /// <param name="assembly">service assembly</param>
         /// <param name="patternSuffix">end of the service nomenclature</param>
-        /// <param name="patternPrefix">starts of the service nomenclature</param>
+        /// <param name="patternPrefix">starts of the service nomenclature (default "I")</param>
         public static void AddServicesScope(IServiceCollection container, Assembly assembly, string patternSuffix, string patternPrefix = "I")
-        {
-            var registrations =
-              from type in assembly.GetExportedTypes()
-              where type.Name.EndsWith(patternSuffix)
-              where type.Name.StartsWith(patternPrefix)
-              where !type.IsInterface
-              where type.GetInterfaces().Any()
-              select new { Implementation = type, Service = type.GetInterfaces().FirstOrDefault() };
-
-            foreach (var reg in registrations)
-                container.AddScoped(reg.Service, reg.Implementation);
-        }
+            => AddServices(container, assembly, patternSuffix, patternPrefix, InjectionTypeValue.Scope);
 
         /// <summary>
         /// Add services as singleton
@@ -36,20 +26,9 @@ namespace HCI.Core.Helper.IoC
         /// <param name="container">service container</param>
         /// <param name="assembly">service assembly</param>
         /// <param name="patternSuffix">end of the service nomenclature</param>
-        /// <param name="patternPrefix">starts of the service nomenclature</param>
+        /// <param name="patternPrefix">starts of the service nomenclature (default "I")</param>
         public static void AddServicesSingleton(IServiceCollection container, Assembly assembly, string patternSuffix, string patternPrefix = "I")
-        {
-            var registrations =
-              from type in assembly.GetExportedTypes()
-              where type.Name.EndsWith(patternSuffix)
-              where type.Name.StartsWith(patternPrefix)
-              where !type.IsInterface
-              where type.GetInterfaces().Any()
-              select new { Implementation = type, Service = type.GetInterfaces().FirstOrDefault() };
-
-            foreach (var reg in registrations)
-                container.AddSingleton(reg.Service, reg.Implementation);
-        }
+            => AddServices(container, assembly, patternSuffix, patternPrefix, InjectionTypeValue.Singleton);
 
         /// <summary>
         /// Add services as transient
@@ -57,19 +36,40 @@ namespace HCI.Core.Helper.IoC
         /// <param name="container">service container</param>
         /// <param name="assembly">service assembly</param>
         /// <param name="patternSuffix">end of the service nomenclature</param>
-        /// <param name="patternPrefix">starts of the service nomenclature</param>
-        public static void AddServicesTransient(IServiceCollection container, Assembly assembly, string patternSuffix, string patternPrefix = "I")
+        /// <param name="patternPrefix">starts of the service nomenclature (default "I")</param>
+        public static void AddServicesTransient(IServiceCollection container, Assembly assembly, string patternSuffix, string patternPrefix = null)
+            => AddServices(container, assembly, patternSuffix, patternPrefix, InjectionTypeValue.Transient);
+
+        private static void AddServices(IServiceCollection container, Assembly assembly, string patternSuffix, string patternPrefix, InjectionTypeValue injectionTypeValue)
         {
+            patternPrefix = string.IsNullOrWhiteSpace(patternPrefix) ? "I" : patternPrefix;
             var registrations =
               from type in assembly.GetExportedTypes()
               where type.Name.EndsWith(patternSuffix)
-              where type.Name.StartsWith(patternPrefix)
               where !type.IsInterface
               where type.GetInterfaces().Any()
-              select new { Implementation = type, Service = type.GetInterfaces().FirstOrDefault() };
+              select new { Implementation = type, Service = type.GetInterfaces().FirstOrDefault(x => x.Name.StartsWith(patternPrefix)) };
 
             foreach (var reg in registrations)
-                container.AddTransient(reg.Service, reg.Implementation);
+                switch (injectionTypeValue)
+                {
+                    case InjectionTypeValue.Scope:
+                        container.AddScoped(reg.Service, reg.Implementation);
+                        break;
+                    case InjectionTypeValue.Singleton:
+                        container.AddSingleton(reg.Service, reg.Implementation);
+                        break;
+                    case InjectionTypeValue.Transient:
+                        container.AddTransient(reg.Service, reg.Implementation);
+                        break;
+                }
+        }
+
+        private enum InjectionTypeValue
+        {
+            Scope,
+            Singleton,
+            Transient,
         }
     }
 }
