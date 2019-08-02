d360d686541524cb2d4697d9a73a188bd411c785
Eduardo Silva <psilva@llip.com>
2019-08-02 16:31:23 -0300

    RegisterFactory

--- src/HCI.Core.Helper/HCI.Core.Helper/IoC/ServicesRegister.cs
+++ src/HCI.Core.Helper/HCI.Core.Helper/IoC/ServicesRegister.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -64,6 +66,46 @@ namespace HCI.Core.Helper.IoC
                         break;
                 }
         }
+
+        /// <summary>
+        /// Factory of group services
+        /// </summary>
+        /// <typeparam name="TFactory">factory implementation</typeparam>
+        /// <typeparam name="TService">abstraction of services</typeparam>
+        /// <param name="container">service container</param>
+        /// <param name="patternSuffix">end of the service nomenclature</param>
+        /// <param name="patternPrefix">starts of the service nomenclature (default "I")</param>
+        /// <param name="callback">key to instance</param>
+        /// <returns></returns>
+        public static TFactory RegisterFactory<TFactory, TService>(IServiceCollection container, string patternSuffix, string patternPrefix, ProductName callback)
+            where TService : class
+            where TFactory : Dictionary<string, Func<TService>>
+        {
+            var factory = Activator.CreateInstance<TFactory>();
+            patternPrefix = string.IsNullOrWhiteSpace(patternPrefix) ? "I" : patternPrefix;
+            var registrations =
+              from type in typeof(TFactory).Assembly.GetExportedTypes()
+              where type.Name.EndsWith(patternSuffix)
+              where !type.IsInterface
+              where type.GetInterfaces().Any()
+              where type.GetInterfaces().Contains(typeof(TService))
+              select new
+              {
+                  Implementation = type,
+                  ProductName = callback?.Invoke(type) ?? type.Name
+              };
+
+            foreach (var reg in registrations)
+                factory.Add(reg.ProductName, () => (TService)Activator.CreateInstance(reg.Implementation));
+            return factory;
+        }
+
+        /// <summary>
+        /// Key for register factory
+        /// </summary>
+        /// <param name="type">type of service</param>
+        /// <returns></returns>
+        public delegate string ProductName(Type type);
 
         private enum InjectionTypeValue
         {
